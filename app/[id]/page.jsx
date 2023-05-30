7ef7e9e76e299286d4287af1ed8990170863cdd7
Bartek Z <bartekzarzyk@gmail.com>
2023-05-30 16:07:07 +0200

    move function outside the component

--- app/[id]/page.jsx
+++ app/[id]/page.jsx
@@ -1,18 +1,18 @@
 import Stripe from 'stripe';
 import ProductLayout from '../components/ProductLayout';
 
-const ProductPage = async ({ searchParams }) => {
-  const gesStripeSingleProduct = async () => {
-    const stripe = new Stripe(process.env.NEXT_SECRET_STRIPE ?? '', {
-      apiVersion: '2020-08-27',
-    });
-    const response = await stripe.prices.list({
-      expand: ['data.product'],
-    });
-    const products = response.data;
-    return products;
-  };
+const gesStripeSingleProduct = async () => {
+  const stripe = new Stripe(process.env.NEXT_SECRET_STRIPE ?? '', {
+    apiVersion: '2020-08-27',
+  });
+  const response = await stripe.prices.list({
+    expand: ['data.product'],
+  });
+  const products = response.data;
+  return products;
+};
 
+const ProductPage = async ({ searchParams }) => {
   const products = await gesStripeSingleProduct();
   const singleProduct = products.find(({ id }) => id === searchParams.id);
 
